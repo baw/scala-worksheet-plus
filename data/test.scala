227b547f9bdb62d981c271bd4f57800c47439682
n.pryanichnikov <n.pryanichnikov@vectordigital.ru>
2014-08-05 12:37:58 +0400

    data

--- data/test.scala
+++ data/test.scala
@@ -11,5 +11,13 @@ object B extends C {
 
 }                                                   // defined module B
 val a = "asd"                                       // a: java.lang.String = asd
-new A()                                             // res2: A = A@5b54a573
-B.a                                                 // res3: Int = 1
+new A()                                             // res11: A = A@73a6068c
+B.a                                                 // res12: Int = 1
+
+
+def f(n:Int) = n * 2                                // f: (n: Int)Int
+f(1000)                                             // res13: Int = 2000
+
+2 / 0                                               // java.lang.ArithmeticException: / by zero
+                                                    // 	at .<init>(<console>:8)
+                                                    // 	at .<clinit>(<console>)
